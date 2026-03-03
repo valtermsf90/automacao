5bc66d6042738e310663905b77deda67ba3834df
Valtermsf90 <machadofilho.ti@gmail.com>
2026-03-03 12:00:47 -0300

    Implement automation script for product registration using PyAutoGUI and pandas

--- automaçao.py
+++ automaçao.py
@@ -1,28 +1,25 @@
 import pyautogui
 import time
-
+import pandas
 
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
-
 pyautogui.press('win')
 pyautogui.write('chrome')
 pyautogui.press('enter')
-time.sleep(3)  # Aguarda o Chrome abrir
-
-pyautogui.hotkey('win', 'up')    # Maximiza janela
+time.sleep(5)  # Aguarda o Chrome abrir
+#pyautogui.hotkey('win', 'down')  # Minimiza janela
+#pyautogui.hotkey('win', 'up')    # Maximiza janela
 
 pyautogui.write(link)
 pyautogui.press('enter')
 time.sleep(5)  # Aguarda a página carregar
 
-pyautogui.click(x=730, y=374)  # Cuidado: coordenada fixa
+pyautogui.click(x=666, y=473)  # Cuidado: coordenada fixa
 pyautogui.write("pythonimpressionador@gmail.com")
 pyautogui.press('tab')
+time.sleep(0.5)
 pyautogui.write("sua senha")  # Troque por uma variável segura
 pyautogui.press('enter')
 time.sleep(3)
-
-import pandas
-
 tabela = pandas.read_csv("produtos.csv")
 print(tabela)
